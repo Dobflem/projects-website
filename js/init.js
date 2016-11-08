9dccccb88dab41801bae3bdf561540ee7e32baae
Russell Hickey <rhickeyjnr@gmail.com>
2016-11-08 12:19:39 +0000

    Add Open Source Licensing

--- js/init.js
+++ js/init.js
@@ -7,9 +7,15 @@
 })(jQuery); // end of jQuery name space
 
 $(document).ready(function() {
+
+  // Set up card descriptions on click
   $(document).on('click', '.card', function() {
     var info = $(this).find(".card-reveal");
     $(info).show();
     $(info).css({"-webkit-transform":"translateY(-100%)"});
   });
+
+  // Make sure all cards are of matching heights
+  $(".p-card").matchHeight();
+
 });
