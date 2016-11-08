603d967684201a776270e0be2d82806e438fc332
Russell Hickey <rhickeyjnr@gmail.com>
2016-11-08 11:32:19 +0000

    Add a description to Sudoku Solver

--- js/init.js
+++ js/init.js
@@ -5,3 +5,11 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+  $(document).on('click', '.card', function() {
+    var info = $(this).find(".card-reveal");
+    $(info).show();
+    $(info).css({"-webkit-transform":"translateY(-100%)"});
+  });
+});
